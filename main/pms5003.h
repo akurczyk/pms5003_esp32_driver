c45e885ff848e109fcf815125ab81b683ce77811
Aleksander Kurczyk <akurczyk000@gmail.com>
2018-01-26 20:08:24 +0100

    Allowing multiple instances

--- main/pms5003.h
+++ main/pms5003.h
@@ -1,29 +1,31 @@
 #include "driver/gpio.h"
 #include "driver/uart.h"
 
-#define PMS_SET		GPIO_NUM_13
-#define PMS_RESET	GPIO_NUM_27
-#define PMS_MODE	GPIO_NUM_26
-#define PMS_PWM		GPIO_NUM_25
-#define PMS_RXD		GPIO_NUM_14
-#define PMS_TXD		GPIO_NUM_12
-#define PMS_RTS		UART_PIN_NO_CHANGE
-#define PMS_CTS		UART_PIN_NO_CHANGE
-#define PMS_UART_BUF_SIZE	128
+typedef struct {
+    gpio_num_t set_pin;
+    gpio_num_t reset_pin;
+    gpio_num_t mode_pin;
+    gpio_num_t rxd_pin;
+    gpio_num_t txd_pin;
+    gpio_num_t rts_pin;
+    gpio_num_t cts_pin;
+    uart_port_t uart_instance;
+    int uart_buffer_size;
+} pms5003_config_t;
 
 typedef struct {
-	int pm1_0_std;
-	int pm2_5_std;
-	int pm10_std;
-	int pm1_0_atm;
-	int pm2_5_atm;
-	int pm10_atm;
-} pms_measurement;
+    int pm1_0_std;
+    int pm2_5_std;
+    int pm10_std;
+    int pm1_0_atm;
+    int pm2_5_atm;
+    int pm10_atm;
+} pms5003_measurement_t;
 
-void pms_setup();
-void pms_make_measurement(pms_measurement* reading);
-int pms_process_data(int len, uint8_t* data, pms_measurement* reading);
-void pms_print_measurement(pms_measurement* reading);
-void pms_clear_measurement(pms_measurement* m);
-void pms_add_measurements(pms_measurement* m1, pms_measurement* m2);
-void pms_div_measurement(pms_measurement* m, int d);
+void pms5003_setup(pms5003_config_t* inst);
+void pms5003_make_measurement(pms5003_config_t* inst, pms5003_measurement_t* reading);
+int pms5003_process_data(int len, uint8_t* data, pms5003_measurement_t* reading);
+void pms5003_print_measurement(pms5003_measurement_t* reading);
+void pms5003_clear_measurement(pms5003_measurement_t* m);
+void pms5003_add_measurements(pms5003_measurement_t* m1, pms5003_measurement_t* m2);
+void pms5003_div_measurement(pms5003_measurement_t* m, int d);
--- main/pms5003_driver_main.c
+++ main/pms5003_driver_main.c
@@ -5,15 +5,23 @@
 
 void app_main()
 {
-	pms_setup();
+    pms5003_config_t pms0 = {
+            .set_pin = GPIO_NUM_13,
+            .reset_pin = GPIO_NUM_27,
+            .mode_pin = GPIO_NUM_26,
+            .rxd_pin = GPIO_NUM_14,
+            .txd_pin = GPIO_NUM_12,
+            .uart_instance = UART_NUM_1,
+            .uart_buffer_size = 128
+    };
+    pms5003_setup(&pms0);
 
-	for(;;)
-	{
-		pms_measurement reading;
-		pms_make_measurement(&reading);
-		pms_print_measurement(&reading);
+    for (;;)
+    {
+        pms5003_measurement_t reading;
+        pms5003_make_measurement(&pms0, &reading);
+        pms5003_print_measurement(&reading);
 
-		fflush(stdout);
-		vTaskDelay(10*1000);
-	}
+        vTaskDelay(10 * 1000);
+    }
 }
